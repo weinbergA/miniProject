cf37174fd27cc7b0a5a2e9d5272abbd6d785636d
Your Name <you@example.com>
2017-12-11 14:41:19 +0200

    Nanny

---
+++ miniProject/BE/Nanny.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BE
+{
+    public class Nanny
+    {
+        int id;
+        string lastName;
+        string firstName;
+        DateTime dateOfBirth;
+        int phoneNumber;
+        string address;
+        bool elevator;
+        int floor;
+        double experienceYears;
+        int maxChildren;
+        double minAgeChildren;
+        double maxAgeChildren;
+        bool hourlyRateAccepting;
+        double hourlyRate;
+        double monthlyRate;
+        bool[] isWorkingToday = new bool[6];
+        DateTime[,] workingHours = new DateTime[2,6];
+        bool tamatHolidays;
+        string reviews;
+        public override string ToString()
+        {
+            string str = "";
+            str += "\nName: " + firstName + lastName;
+            str += "\nrate per mounth: " + monthlyRate;
+            str += "/nworking days:";
+            for (int i = 0; i < 6; i++)
+            {
+                if(isWorkingToday[i])
+                {
+                    str += (workingHours[0, i]).DayOfWeek + ": ";
+                    str += (workingHours[0, i]).Hour + " - ";
+                    str += (workingHours[1, i]).Hour;
+                }
+            }
+            return str;
+        }
+    }
+}
