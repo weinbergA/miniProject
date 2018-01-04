cee683706bc00248b79029ed28513d2f07a817e8
aweinber <aweinbe@g.jct.ac.il>
2018-01-04 20:28:34 +0200

    04/12

--- miniProject/PLWPF/NewNanny.xaml.cs
+++ miniProject/PLWPF/NewNanny.xaml.cs
@@ -27,11 +27,26 @@ namespace PLWPF
             nanny = new BE.Nanny();
             bl = BL.FactoryBL.GetBL();
             this.DataContext = nanny;
+            
+            
         }
 
         
         private void nanny_join_Click(object sender, RoutedEventArgs e)
         {
+            nanny.workingHours[0, 0] = enterHoures_0.Value.Value.TimeOfDay;
+            nanny.workingHours[0, 1] = leaveHoures_0.Value.Value.TimeOfDay;
+            nanny.workingHours[1, 0] = enterHoures_1.Value.Value.TimeOfDay;
+            nanny.workingHours[1, 1] = leaveHoures_1.Value.Value.TimeOfDay;
+            nanny.workingHours[2, 0] = enterHoures_2.Value.Value.TimeOfDay;
+            nanny.workingHours[2, 1] = leaveHoures_2.Value.Value.TimeOfDay;
+            nanny.workingHours[3, 0] = enterHoures_3.Value.Value.TimeOfDay;
+            nanny.workingHours[3, 1] = leaveHoures_3.Value.Value.TimeOfDay;
+            nanny.workingHours[4, 0] = enterHoures_4.Value.Value.TimeOfDay;
+            nanny.workingHours[4, 1] = leaveHoures_4.Value.Value.TimeOfDay;
+            nanny.workingHours[5, 0] = enterHoures_5.Value.Value.TimeOfDay;
+            nanny.workingHours[5, 1] = leaveHoures_5.Value.Value.TimeOfDay;
+
             try
             {
                 bl.addNanny(nanny);
